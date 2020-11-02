ec95cbf75ce04b284a72be1335be60002f128696
Tomotom0 <arukuki4@outlook.jp>
2020-11-02 13:07:32 +0900

    auto commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# HackmdTOC_alwaysView
-This extension can enable you to view HackMD's TOC in sidebar even in edit or split-view mode.
+Chrome拡張の雛形
+========================
+
+概要
+-----
+
+これをダウンロードかcloneして、書き換えてください。
+
+
+各種ドキュメント/サンプルページ
+-----------------------------------
+
+- [使い方](http://qiita.com/waterada/private/f8a53677a46c131a3734)
+- [サンプルページ](https://waterada.github.io/chrome-ex-template/test/example.html)
+- [ChEx.js のドキュメント](ChEx-doc.md)
+- [単体テスト](https://waterada.github.io/chrome-ex-template/test/test.html)
+
+
+License
+-----------------------------------
+
+MIT
---
+++ src/content_script.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    const scroll_ver=$(".CodeMirror-overlayscroll-vertical");
+    scroll_ver.css("width","10px");
+
+    const toc=$("div .toc:eq(1)").clone().addClass("expand");
+    const toc_menu=$(".toc-menu:eq(1)");
+    const div=$("<div>", {"class":"ui-toc-dropdown ui-affix-toc unselectable hidden-print scrollspy-view"})
+    .css({"max-height":"", "background-color":"transparent", overflow:"hidden", width:"120px", padding:"10px", margin:"",
+     right:"10px" , top:"0", width:"120px", "border":"none", height:"100%", top:"0", padding:"0", "z-index":"10"});
+    div.append(toc);
+    div.append(toc_menu);
+    sc.append(div);
+    $(".toc:eq(0)", div).css({"max-height":"", "padding":"0", margin:"0","background":"white", opacity:"0.3","border":"none",
+      height:"", "top":"14%"})
+    $(".toc-menu", div).css({"max-height":"", "padding":"0", margin:"0","background":"white", opacity:"0.3","border":"none",
+      height:"", "top":"1%"})
+
+    const scbar=$(".CodeMirror-overlayscroll-vertical div:eq(0)");
+    scbar.css({opacity:"0.7","max-height":"50px", "background-color":"grey", "pointer-events":"none"}).css({"pointer-events":""});
+
+});
